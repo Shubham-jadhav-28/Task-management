ca76cbc3f8aa5244611508f3f4f43aff188a1e10
Shubham-jadhav-28 <shubham.jadhav@waiin.com>
2025-04-07 18:55:37 +0530

    All set

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-// Removed redundant import of NavbarComponent
+
 
 @Component({
   selector: 'app-navbar',
